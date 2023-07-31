8dc34f49ee38b7cb91655f28a5c9920011433c76
Abuzkah <110993144+Abuzkah@users.noreply.github.com>
2023-07-31 09:57:09 -0400

    Changes Updated

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Create a handler for 404 errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
